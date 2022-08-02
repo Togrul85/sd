f3f9b46f003914aabb735d17bbd9e4b92281f0c4
unknown <toghrul@code.edu.az>
2022-08-02 17:16:54 +0400

    firsrtwork

---
+++ firstwork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace firstwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
